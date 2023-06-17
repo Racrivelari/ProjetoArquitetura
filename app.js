48edaee5005412e3b929a515a60a754fd47b036f
AsianJack <lucastanakinha100@hotmail.com>
2023-06-17 14:43:59 -0300

    adicao das rotas

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+var path = require('path');
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000; 
-
+// Configurar o diretório estático para servir arquivos HTML
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+// Importar o arquivo de rotas
 const routes = require('./routes/routes.js');
+
+// Associar o roteador às rotas principais
 app.use('/', routes);
 
 app.listen(port, () => {
--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Página Home' );
+  res.sendFile('home.html', { root: 'views' });
 });
 
-// router.get('/', (req, res) => {
-//     res.render('home', {
-//       title: 'Home',
-//     });
-//   });
+router.get('/sobre', (req, res) => {
+  res.sendFile('sobre.html', { root: 'views' });
+});
+
+router.get('/negocio', (req, res) => {
+  res.sendFile('negocio.html', { root: 'views' });
+});
+
+router.get('/somos', (req, res) => {
+  res.sendFile('somos.html', { root: 'views' });
+});
+
+router.get('/cliente', (req, res) => {
+  res.sendFile('cliente.html', { root: 'views' });
+});
+
+router.get('/contato', (req, res) => {
+  res.sendFile('contato.html', { root: 'views' });
+});
 
 module.exports = router;
