7056fc3d70a8818dd827e6a97fd500b7fd908c6e
Ramon Crivelari <ramon.crivelari@gmail.com>
2023-06-18 01:44:42 -0300

    modularizacao scripts tela Pedidos

---
+++ public/javascripts/libraries.js
@@ -0,0 +1,5 @@
+import List from 'https://cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js';
+import jsPDF from 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js';
+import jsAutoTable from 'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.16/jspdf.plugin.autotable.min.js';
+
+export { List, jsPDF, jsAutoTable };
